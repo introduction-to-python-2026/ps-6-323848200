b03f44a88179361983f1d95744b78edfd7567a01
323848200 <hadarfarkas@gmail.com>
2026-01-23 15:25:46 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -2,6 +2,15 @@ def create_codon_dict(file_path):
     codon_dict = {}
     with open(file_path, 'r') as file:
         for line in file:
-            codon, amino_acid, single_letter, full_name = line.split()
-            codon_dict[codon] = single_letter
-    return codon_dict
+            line = line.strip()
+            
+            if not line:
+                continue
+                
+            parts = line.split()
+            
+            if len(parts) >= 4:
+                codon, amino_acid, single_letter, full_name = parts[:4]
+                codon_dict[codon] = single_letter
+                
+    return codon_dict
